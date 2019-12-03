d32ca8313ab16c14520399c79763993764e827dd
helmax-y <max_rad@ukr.net>
2019-12-03 14:03:22 +0200

    Solution

---
+++ precache-manifest.ea90c393c729db0bb4fbd8ee77470cce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eec8afe7a1717f3e099210989f6428d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c42cb47e97484b242ef2",
+    "url": "./static/css/main.93e4d0b0.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "./static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "c42cb47e97484b242ef2",
+    "url": "./static/js/main.03bcc56b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
